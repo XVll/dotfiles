0601382f9cbffcbcdddeb8e4eea01a06c306a164
XVll <onur03@gmail.com>
2023-07-08 16:37:54 +0300

    Install script update

--- install.sh
+++ install.sh
@@ -1,55 +1,69 @@
 #!/bin/sh
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
 
 # curl -fsSL https://raw.githubusercontent.com/XVll/dotfiles/main/install.sh | bash
 
 if ! xcode-select --print-path &> /dev/null
 then
-    echo "Command Line Tools not found, installing now..."
-    # Installing Command Line Tools
+    echo "${BLUE}Command Line Tools not found, installing now...${NC}"
+
     xcode-select --install
+
+    # Wait until the Command Line Tools are installed
+    until xcode-select --print-path &> /dev/null; do
+        echo "${YELLOW}Waiting for Command Line Tools installation to complete...${NC}"
+        sleep 5
+    done
+
+    echo "${GREEN}Command Line Tools installed successfully!${NC}"
 else
-    echo "Command Line Tools already installed!"
+    echo "${GREEN}Command Line Tools already installed!${NC}"
 fi
 
 if ! command -v brew &> /dev/null
 then
-    echo "Homebrew could not be found, installing now..."
+    echo "${BLUE}Homebrew could not be found, installing now...${NC}"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/fx/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 else
-    echo "Homebrew is already installed!"
+    echo "${GREEN}Homebrew is already installed!${NC}"
 fi
 
 if ! brew list --cask 1password &> /dev/null
 then
-    echo "1Password could not be found, installing now..."
+    echo "${BLUE}1Password could not be found, installing now...${NC}"
     brew install --cask 1password
 else
-    echo "1Password is already installed!"
+    echo "${GREEN}1Password is already installed!${NC}"
 fi
 
 if ! command -v op &> /dev/null
 then
-    echo "1Password CLI could not be found, installing now..."
+    echo "${BLUE}1Password CLI could not be found, installing now...${NC}"
     brew install 1password-cli
 else
-    echo "1Password CLI is already installed!"
+    echo "${GREEN}1Password CLI is already installed!${NC}"
 fi
 
 if ! command -v chezmoi &> /dev/null
 then
-    echo "Chezmoi could not be found, installing now..."
+    echo "${BLUE}Chezmoi could not be found, installing now...${NC}"
     brew install chezmoi
 else
-    echo "Chezmoi is already installed!"
+    echo "${GREEN}Chezmoi is already installed!${NC}"
 fi
 
-echo "Setup your 1Password account in the desktop application and press any key to continue"
-echo "Ensure Full Disk Access is granted to Terminal, some preferences requires."
-read -n 1 -s -r
+echo "${YELLOW}Ensure Full Disk Access is granted to Terminal, some preferences requires.${NC}"
 
+while ! op account get > /dev/null 2>&1; do
+    echo "${RED}Configure and sign in to your 1Password account in the desktop application then press any key to continue${NC}"
+    read -n 1 -s -r
+done
 
-echo "Starting chezmoi..."
+echo "${BLUE}Starting chezmoi...${NC}"
 chezmoi init XVll --apply --ssh
-echo "Done!"
+echo "${GREEN}Done!${NC}"
