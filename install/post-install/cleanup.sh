02d3df8bbcd37f0fe6774e992b1933b55a64e6bc
xvll <onur03@gmail.com>
2026-04-12 00:25:13 +0300

    Remove disable-mkinitcpio: fragile, minimal gain on CachyOS

--- install/post-install/cleanup.sh
+++ install/post-install/cleanup.sh
@@ -1,24 +1,6 @@
 #!/usr/bin/env bash
 source "$(dirname "${BASH_SOURCE[0]}")/../helpers.sh"
 
-info "Re-enabling mkinitcpio hooks"
-
-# Restore the hooks we disabled in preflight and rebuild initramfs once
-if [[ -f /usr/share/libalpm/hooks/90-mkinitcpio-install.hook.disabled ]]; then
-  sudo mv /usr/share/libalpm/hooks/90-mkinitcpio-install.hook.disabled \
-          /usr/share/libalpm/hooks/90-mkinitcpio-install.hook
-fi
-
-if [[ -f /usr/share/libalpm/hooks/60-mkinitcpio-remove.hook.disabled ]]; then
-  sudo mv /usr/share/libalpm/hooks/60-mkinitcpio-remove.hook.disabled \
-          /usr/share/libalpm/hooks/60-mkinitcpio-remove.hook
-fi
-
-# Rebuild initramfs once for all installed kernels
-sudo mkinitcpio -P
-
-ok "mkinitcpio hooks restored and initramfs rebuilt"
-
 info "Removing orphaned packages"
 
 ORPHANS=$(paru -Qdtq 2>/dev/null || true)
