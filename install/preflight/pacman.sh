92f07b5c0815015f2b038c3d11ba3c785cc482bf
xvll <onur03@gmail.com>
2026-04-11 23:51:40 +0300

    Simplify pacman.sh: just pacman -Syyuu, drop irrelevant config

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
 source "$(dirname "${BASH_SOURCE[0]}")/../helpers.sh"
 
-info "Configuring pacman"
+info "Updating system"
 
-# Enable parallel downloads and color output (commented out by default in Arch)
-sudo sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
-sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
-
-# Sync package databases and upgrade everything before we start installing
-# Prevents conflicts between old package versions and new ones we're about to install
+# Full sync + upgrade before installing anything
+# Prevents version conflicts between existing and new packages
 sudo pacman -Syyuu --noconfirm
 
-timedatectl set-ntp true
-sudo timedatectl set-timezone Europe/Istanbul
-
-ok "Pacman configured and system up to date"
+ok "System up to date"
