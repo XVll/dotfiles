d4e8f026300d0a18a449bac37abca5e0660d9b0f
xvll <onur03@gmail.com>
2026-04-12 00:22:48 +0300

    paru.sh: simplify, paru confirmed in CachyOS repo

--- install/preflight/paru.sh
+++ install/preflight/paru.sh
@@ -4,16 +4,6 @@ need_user
 
 info "Installing paru"
 
-if pacman -Si paru &>/dev/null; then
-  # Available in repo (CachyOS ships paru in their repo)
-  sudo pacman -S --needed --noconfirm paru
-else
-  # Fall back to building from AUR
-  cd /tmp
-  git clone https://aur.archlinux.org/paru.git
-  cd paru
-  makepkg -si --noconfirm
-  cd ~
-fi
+sudo pacman -S --needed --noconfirm paru
 
 ok "paru installed"
